ed600a83d78d6a72d2c2e0e3046282b2b7273796
NadyaB90 <lady.nadya20@mail.ru>
2022-07-27 19:43:38 +0500

    Initial commit

---
+++ NewsPaper/news/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+
+class PostsList(ListView):
+    model = Post
+    template_name = 'news.html'
+    ordering = ['-created_date']
+    context_object_name = 'news'
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
